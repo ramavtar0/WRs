94c4669c1afebe5dbb6d0e07af8880839d610e5f
Ram Avtar <ram.avtar0@gmail.com>
2024-11-10 22:06:56 +0530

    Add project files.

---
+++ WRs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WRs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
